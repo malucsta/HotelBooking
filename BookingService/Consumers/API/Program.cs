1be73b0904f2790d36e77bf263e6d7061e8e3b19
Maria Luísa <araujocostamalu@gmail.com>
2022-09-12 16:15:35 -0300

    connecting to db and adding first migrations

--- BookingService/Consumers/API/Program.cs
+++ BookingService/Consumers/API/Program.cs
@@ -1,8 +1,15 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefautConnection");
+builder.Services.AddDbContext<HotelDbContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
